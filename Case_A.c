620ab6c70bf7632dbd5f1dc48ab647b8becb87ba
MohabAshrafz <MohabAshrafz7@gmail.com>
2022-05-25 00:31:46 +0200

    Case_A

---
+++ Case_A.c
@@ -0,0 +1,18 @@
+#include "tm4c123gh6pm.h"
+#include "LCD.h"
+#include "Functions.h"
+
+void Case_A()
+{
+		unsigned char time[] = {'0','1','0','0'};				
+		LCD_cmd(0x01);					//Clear Display
+		Cursor_Pos(1,5);
+
+		LCD_write_String("Popcorn"); 
+		Display_Timer(&time[0]);		
+		
+		while( (GPIO_PORTF_DATA_R & 0x01) != 0){}		//wait untill switch 2 is pressed
+		Timer(&time[0]);
+
+}
+
---
+++ case_a.d
@@ -0,0 +1,2 @@
+.\case_a.o: Case_A.c
+.\case_a.o: tm4c123gh6pm.h
