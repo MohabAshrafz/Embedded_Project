54d9e2eed61b0bb9f69677ce8129922573bbac87
MohabAshrafz <MohabAshrafz7@gmail.com>
2022-05-24 12:01:58 +0200

    ports init .h

---
+++ Ports_Init.h
@@ -0,0 +1,15 @@
+
+
+void SystemInit(void);
+
+void Keypad_Init(void);
+
+void PortA_Init(void);
+
+void PortE_Init(void);
+
+void PortB_Init(void);
+
+void PortC_Init(void);
+
+
--- kpd.d
+++ kpd.d
@@ -1,2 +1,3 @@
 .\kpd.o: KPD.c
 .\kpd.o: tm4c123gh6pm.h
+.\kpd.o: Ports_Init.h
